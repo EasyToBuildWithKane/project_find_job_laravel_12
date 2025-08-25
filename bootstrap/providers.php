d5b57f7811bebe6544e6c11bdd090c0c4bb50cc0
Nguyễn Khoa <134806583+ResumeKNTech@users.noreply.github.com>
2025-08-25 11:33:37 +0700

    Bonus: Add Package To Project

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,10 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    Barryvdh\Debugbar\ServiceProvider::class,
+    Barryvdh\DomPDF\ServiceProvider::class,
+    Maatwebsite\Excel\ExcelServiceProvider::class,
+    Yajra\DataTables\ButtonsServiceProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
 ];
